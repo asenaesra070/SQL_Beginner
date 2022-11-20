379729705105d5fa9749f8f66c20058f6db3a32f
Asena Esra Güler <74676746+asenaesra070@users.noreply.github.com>
2022-11-20 21:48:26 +0300

    Add files via upload

---
+++ lecture 2.sql
@@ -0,0 +1,20 @@
+create database cats1;
+select cats1;
+
+
+
+create table cats1(
+name varchar(100),
+breed varchar(100),
+age int
+);
+desc cats1;
+CREATE TABLE pastries
+  (
+    name VARCHAR(50),
+    quantity INT
+  );
+DESC pastries;
+DROP TABLE PASTRIES;
+
+
