21619ee94d84bc3acf332567772bcbe7842df96d
Asena Esra Güler <74676746+asenaesra070@users.noreply.github.com>
2023-06-15 16:59:21 +0300

    Add files via upload

---
+++ REVISITING DATA TYPES.sql
@@ -0,0 +1,26 @@
+-- REVISITING DATA TYPES
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+ 
+SELECT * FROM people;
+SELECT name, birthdate FROM people;
+SELECT name, DAY(birthdate) FROM people;
+SELECT name, birthdate, DAY(birthdate) FROM people;
+SELECT name, birthdate, DAYNAME(birthdate) FROM people;
+SELECT name, birthdate, DAYOFWEEK(birthdate) FROM people;
+SELECT name, birthdate, DAYOFYEAR(birthdate) FROM people;
+SELECT name, birthtime, DAYOFYEAR(birthtime) FROM people;
+SELECT name, birthdt, DAYOFYEAR(birthdt) FROM people;
+SELECT name, birthdt, MONTH(birthdt) FROM people;
+SELECT name, birthdt, MONTHNAME(birthdt) FROM people;
+SELECT name, birthtime, HOUR(birthtime) FROM people;
+SELECT name, birthtime, MINUTE(birthtime) FROM people;
+SELECT CONCAT(MONTHNAME(birthdate), ' ', DAY(birthdate), ' ', YEAR(birthdate)) FROM people;
+SELECT DATE_FORMAT(birthdt, 'Was born on a %W') FROM people;
+SELECT DATE_FORMAT(birthdt, '%m/%d/%Y') FROM people;
+SELECT DATE_FORMAT(birthdt, '%m/%d/%Y at %h:%i') AS CORTTIME FROM people;
